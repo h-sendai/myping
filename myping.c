c4b01b5762a26d06acbcecf498c0509fd85e1063
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2020-12-23 14:58:35 +0900

    separate getaddrinfo() function

--- myping.c
+++ myping.c
@@ -47,6 +47,32 @@ int use_raw_sock = 1;
 int use_ping_sock = 0;
 int debug = 0;
 
+int set_sockaddr_in(struct sockaddr_in *sa, char *remote_host)
+{
+    struct sockaddr_in *resaddr;
+    struct addrinfo    hints;
+    struct addrinfo    *res;
+    int errorcode;
+
+    res = 0;
+    memset((char *)&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_RAW;
+    hints.ai_protocol = 0;
+    if ( (errorcode = getaddrinfo(remote_host, 0, &hints, &res)) != 0) {
+        fprintf(stderr, "%s\n", gai_strerror(errorcode));
+        exit(1);
+    }
+
+    resaddr = (struct sockaddr_in *)res->ai_addr;
+    memset((char *)sa, 0, sizeof(struct sockaddr_in));
+    sa->sin_family = AF_INET;
+    sa->sin_port   = htons(0);
+    sa->sin_addr   = resaddr->sin_addr;
+    freeaddrinfo(res);
+    
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -81,30 +107,10 @@ int main(int argc, char *argv[])
         usage();
         exit(1);
     }
+    char *remote_host = argv[0];
 
     struct sockaddr_in sa_send;
-    struct sockaddr_in *resaddr;
-    struct addrinfo    hints;
-    struct addrinfo    *res;
-    int errorcode;
-    // socklen_t salen = sizeof(struct sockaddr_in);
-
-    res = 0;
-    memset((char *)&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_RAW;
-    hints.ai_protocol = 0;
-    if ( (errorcode = getaddrinfo(argv[0], 0, &hints, &res)) != 0) {
-        fprintf(stderr, "%s\n", gai_strerror(errorcode));
-        exit(1);
-    }
-
-    resaddr = (struct sockaddr_in *)res->ai_addr;
-    memset((char *)&sa_send, 0, sizeof(sa_send));
-    sa_send.sin_family = AF_INET;
-    sa_send.sin_port   = htons(0);
-    sa_send.sin_addr   = resaddr->sin_addr;
-    freeaddrinfo(res);
+    set_sockaddr_in(&sa_send, remote_host);
 
     int sockfd;
     if (use_raw_sock) {
@@ -124,6 +130,8 @@ int main(int argc, char *argv[])
 
     unsigned char sendbuf[8000];
     unsigned char recvbuf[8000];
+    memset(sendbuf, 0, sizeof(sendbuf));
+    memset(recvbuf, 0, sizeof(recvbuf));
     int datalen = 56;
     // struct icmp is defined in /usr/include/netinet/ip_icmp.h
     // data member
@@ -136,18 +144,25 @@ int main(int argc, char *argv[])
 
     useconds_t interval_usec = strtod(interval_string, NULL)*1000000.0;
 
+    int seq_num = 0;
+    pid_t pid = getpid();
     for ( ; ; ) {
+        seq_num ++;
         struct icmp *icmp = (struct icmp *)sendbuf;
         icmp->icmp_type = ICMP_ECHO;
         icmp->icmp_code = 0;
-        icmp->icmp_seq  = 0xfeed;
-        icmp->icmp_id   = getpid();
+        icmp->icmp_seq  = htons(seq_num);
+        icmp->icmp_id   = htons(pid);
         memset(icmp->icmp_data, 0xFF, datalen);
-        gettimeofday((struct timeval *)icmp->icmp_data, NULL);
+        gettimeofday((struct timeval *)&(icmp->icmp_data), NULL);
 
         int len = 8 + datalen; /* 8: icmp header size */
         icmp->icmp_cksum = 0;
         icmp->icmp_cksum = in_cksum((unsigned short *)icmp, len);
+        if (debug) {
+            fprintf(stderr, "---> sendbuf\n");
+            print_bytes(sendbuf, len);
+        }
 
         size_t n;
         n = sendto(sockfd, sendbuf, len, 0, (struct sockaddr *)&sa_send, sizeof(sa_send));
@@ -164,15 +179,23 @@ int main(int argc, char *argv[])
             err(1, "recvfrom");
         }
         if (debug) {
-            printf("%ld bytes received\n", n);
+            fprintf(stderr, "---> recvbuf\n");
+            fprintf(stderr, "%ld bytes received\n", n);
             print_bytes(recvbuf, n);
         }
 
         struct timeval *tv0_p, tv0, tv1, rtt;
         int tv_in_recvbuf = 8; /* 8: icmp header */
+        int type_pos = 0;
         if (use_raw_sock) {
             tv_in_recvbuf += 20; /* XXX: 20: IP header.  should be decode IP header length value */
+            type_pos += 20;
         }
+
+        //unsigned char *type_p = &recvbuf[type_pos];
+        //if (*type_p != ICMP_ECHOREPLY) {
+            //continue;
+        //}
 
         tv0_p = (struct timeval *)&recvbuf[tv_in_recvbuf];
         tv0 = *tv0_p;
@@ -189,6 +212,9 @@ int main(int argc, char *argv[])
             tv0.tv_sec, tv0.tv_usec,
             tv1.tv_sec, tv1.tv_usec);
 
+        if (debug) {
+            fprintf(stderr, "sleep\n");
+        }
         usleep(interval_usec);
     }
     return 0;
